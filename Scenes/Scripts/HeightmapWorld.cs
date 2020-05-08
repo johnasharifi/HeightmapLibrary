776ed8d6ec27039648cb09056822d0b7d9bccfe2
johnasharifi <johnasharifi@gmail.com>
2020-05-07 21:09:01 -0700

    Stub for world-scale texture from heightmap
    Larger scale will allow for better visualization with other world-scale objects. Later gameObjects will move around world

---
+++ Scenes/Scripts/HeightmapWorld.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class HeightmapWorld : MonoBehaviour
+{
+    [SerializeField] private Renderer rend;
+    [SerializeField, Range(64, 256)] private int scale = 128;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (rend == null) rend = GetComponent<Renderer>();
+
+        GenerateMap();
+    }
+
+    void GenerateMap()
+    {
+        Heightmap h = new Heightmap(scale, scale, 10.0f);
+
+        Heightmap rivers = new Heightmap(scale, scale, 1.0f);
+        bool[,] isRiver = Heightmap.AND(0.40f < rivers, rivers < 0.45f);
+        
+        Texture2D tex = (Texture2D)h;
+
+        for (int i = 0; i < scale; i++)
+        {
+            for (int j = 0; j < scale; j++)
+            {
+                if (isRiver[i, j])
+                    tex.SetPixel(i, j, Color.blue);
+                else
+                    tex.SetPixel(i, j, Color.white);
+            }
+        }
+        tex.filterMode = FilterMode.Point;
+        tex.Apply();
+
+        rend.material.mainTexture = tex;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
