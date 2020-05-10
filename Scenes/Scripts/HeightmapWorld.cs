f25ca4986f52d81ea179a3e8f49674d9c42b0028
johnasharifi <johnasharifi@gmail.com>
2020-05-09 21:30:32 -0700

    Add demo scene for world-scale heightmap texture
    HeightmapWorld will contain basic construction script for a low res bitmap style map

--- Scenes/Scripts/HeightmapWorld.cs
+++ Scenes/Scripts/HeightmapWorld.cs
@@ -21,8 +21,22 @@ public class HeightmapWorld : MonoBehaviour
         Heightmap h = new Heightmap(scale, scale, 10.0f);
 
         Heightmap rivers = new Heightmap(scale, scale, 1.0f);
-        bool[,] isRiver = Heightmap.AND(0.40f < rivers, rivers < 0.45f);
+        bool[,] isRiver = Heightmap.AND(0.43f < rivers, rivers < 0.45f);
         
+        Heightmap exterior = new Heightmap(Heightmap.GetExteriorWeight(scale, scale));
+        Heightmap interior = new Heightmap(scale, scale, 10.0f);
+        float[,] exteriorAlpha = Heightmap.GetExteriorWeight(scale, scale);
+        Heightmap combined_radial = Heightmap.Blend(exterior, interior, exteriorAlpha);
+        bool[,] isExterior = combined_radial > 0.5f;
+
+        Heightmap lattice = new Heightmap(scale, scale, 10.0f);
+        bool[,] isLattice = Heightmap.AND(0.40f < lattice, lattice < 0.50f);
+
+        Heightmap minerals = new Heightmap(scale, scale, 10.0f);
+        bool[,] isForest = Heightmap.AND(0.3f < minerals, minerals < 0.4f);
+        bool[,] isPlains = Heightmap.AND(0.5f < minerals, interior < 0.6f);
+        bool[,] isMountain = Heightmap.AND(0.4f < interior, interior < 0.5f);
+
         Texture2D tex = (Texture2D)h;
 
         for (int i = 0; i < scale; i++)
@@ -31,6 +45,16 @@ public class HeightmapWorld : MonoBehaviour
             {
                 if (isRiver[i, j])
                     tex.SetPixel(i, j, Color.blue);
+                else if (isLattice[i, j])
+                    tex.SetPixel(i, j, Color.white);
+                else if (isExterior[i, j])
+                    tex.SetPixel(i, j, Color.red);
+                else if (isMountain[i, j])
+                    tex.SetPixel(i, j, Color.grey);
+                else if (isForest[i, j])
+                    tex.SetPixel(i, j, Color.green);
+                else if (isPlains[i, j])
+                    tex.SetPixel(i, j, Color.yellow);
                 else
                     tex.SetPixel(i, j, Color.white);
             }
