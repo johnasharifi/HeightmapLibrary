ab4ac454b87ed088fcf95ccaff93651f890da1eb
johnasharifi <johnasharifi@gmail.com>
2020-05-19 21:27:44 -0700

    Allow visualization of heightmaps as textures
    Made Heightmap a serializable type. Implemented PropertyDrawer for Heightmap. Added test scene for viewing a Heightmap property of a class.

---
+++ Editor/HeightmapDrawer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+// Heightmap drawn using IMGUI
+[CustomPropertyDrawer(typeof(Heightmap))]
+public class HeightmapDrawer : PropertyDrawer
+{
+    const float heightInEditorLines = 5f;
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return EditorGUIUtility.singleLineHeight * heightInEditorLines;
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        // Using BeginProperty / EndProperty on the parent property means that
+        // prefab override logic works on the entire property.
+        EditorGUI.BeginProperty(position, label, property);
+        
+        Heightmap h = (Heightmap) fieldInfo.GetValue(property.serializedObject.targetObject);
+        Texture2D tex = (Texture2D)h;
+        tex.filterMode = FilterMode.Point;
+        tex.Apply();
+        EditorGUI.DrawPreviewTexture(position, tex);
+
+        EditorGUI.EndProperty();
+    }
+}
---
+++ Scenes/DemoPropertyDrawer/DemoPropertyHeightmap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The purpose of this class is to demonstrate 
+/// </summary>
+#if UNITY_EDITOR
+[ExecuteInEditMode]
+#endif
+public class DemoPropertyHeightmap : MonoBehaviour
+{
+    [SerializeField] Heightmap map;
+
+    private void OnEnable()
+    {
+        // serialized map property is automatically drawn by HeightmapDrawer
+        map = new Heightmap(256, 256, 10);
+    }
+}
