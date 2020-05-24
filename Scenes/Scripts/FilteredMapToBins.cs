4e535728c9af98bd4375e5b89d4f5f673b8b25a5
johnasharifi <johnasharifi@gmail.com>
2020-05-23 23:45:12 -0700

    Transition from a Heightmap of fixed floats to a filtered map
    Filtered map will be a collection of pairs of keys and associated collections of points.
    
    This accelerates queries into map by limiting sample space to only potenial remap indexes.
    This also supports explicit mapping from one class to another; heightmap had issues where AND, comparisons, etc. had to be added using compositions of methods.

---
+++ Scenes/Scripts/FilteredMapToBins.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FilteredMapToBins : MonoBehaviour
+{
+    [SerializeField] private Renderer rend;
+
+    [Range(64, 512)]
+    [SerializeField] private int dims;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FilteredMap map = new FilteredMap(dims, dims);
+
+        var perlinMap = FilterMapFactory.GetPerlinFilter(dims, dims, 1.0f, 0.5f);
+        var perlinMap2 = FilterMapFactory.GetPerlinFilter(dims, dims, 10.0f, 0.5f);
+
+        map.MapFromTo(-1, 0, 1, perlinMap);
+        map.MapFromTo(0, 2, 3, perlinMap2);
+
+        Texture2D tex = new Texture2D(dims, dims);
+        for (int i = 0; i < dims; i++)
+        {
+            for (int j = 0; j < dims; j++)
+            {
+                int ind = map[i, j];
+                if (ind == 0)
+                    tex.SetPixel(i, j, Color.red);
+                else if (ind == 1)
+                    tex.SetPixel(i, j, Color.black);
+                else if (ind == 2)
+                    tex.SetPixel(i, j, Color.blue);
+                else if (ind == 3)
+                    tex.SetPixel(i, j, Color.magenta);
+                else
+                {
+                    Debug.LogFormat("ind at position {0}, {1} is {2}", i, j, ind);
+                    tex.SetPixel(i, j, Color.white);
+                }
+                    
+            }
+        }
+        tex.filterMode = FilterMode.Point;
+        tex.Apply();
+
+        rend.material.mainTexture = tex;
+    }
+    
+}
---
+++ Scripts/FilteredMapUtilities.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public static class FilterMapFactory
+{
+    public static System.Func<int, int, bool> GetPerlinFilter(int dim0, int dim1, float perlinScale, float threshold)
+    {
+        // params: dim2, perlinScale, threshold, xy offsets
+        float xoff0 = Random.Range(0f, 1000f);
+        float yoff0 = Random.Range(0f, 1000f);
+        float xoff1 = Random.Range(0f, 1000f);
+        float yoff1 = Random.Range(0f, 1000f);
+
+        System.Func<int, int, bool> filter = (int i, int j) =>
+        {
+            float f0 = Mathf.PerlinNoise((i * 1.0f / dim0 + xoff0) * perlinScale, (j * 1.0f / dim1 + yoff0));
+            float f1 = Mathf.PerlinNoise((i * 1.0f / dim0 + xoff1) * perlinScale, (j * 1.0f / dim1 + yoff1));
+
+            return (f0 + f1) / 2 < threshold;
+        };
+
+        return (filter);
+    }
+}
