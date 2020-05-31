730d768f4a308ec6d7443f63672053212b991aa7
johnasharifi <johnasharifi@gmail.com>
2020-05-31 16:46:47 -0700

    Renamed FilteredMap content to Heightmap
    FilteredMapDemoScene becomes HeightmapToTextureScene to better reflect scene's function as a demo for heightmap -> T2D conversion. Similar motive for renaming FilteredMapToBins

--- Scenes/Scripts/HeightmapToTexture.cs
+++ Scenes/Scripts/HeightmapToTexture.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FilteredMapToBins : MonoBehaviour
+public class HeightmapToTexture : MonoBehaviour
 {
     [SerializeField] private Renderer rend;
 
