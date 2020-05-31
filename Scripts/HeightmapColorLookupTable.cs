e6c1682adf765b013c09db893138420a265424f6
johnasharifi <johnasharifi@gmail.com>
2020-05-30 22:30:44 -0700

    Include ColorLUT implementation
    Other scripts recruit HeightmapColorLUT. Provide script in commit

---
+++ Scripts/HeightmapColorLookupTable.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Serializable class for defining colors of biomes in a Heightmap.
+/// 
+/// Important for defining a custom UI for this serializable type.
+/// </summary>
+[SerializeField]
+public class HeightmapColorLookupTable
+{
+    private HashSet<int> uncontainedKeyCollection = new HashSet<int>();
+    private Dictionary<int, Color> mapper = new Dictionary<int, Color>();
+
+    public HeightmapColorLookupTable(Dictionary<int, Color> _mapping)
+    {
+        // acquire definitions from user-specified mapping
+        mapper = _mapping;
+    }
+
+    public Color this[int i]
+    {
+        get
+        {
+            if (mapper.ContainsKey(i))
+            {
+                return mapper[i];
+            }
+            if (!uncontainedKeyCollection.Contains(i))
+            {
+                uncontainedKeyCollection.Add(i);
+                Debug.LogErrorFormat("Undefined color mapping for {0}", i);
+            }
+            return default;
+        }
+    }
+}
