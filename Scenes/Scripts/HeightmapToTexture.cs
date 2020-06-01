f3dedfe5d0f499971d4040b88dd19e0093cd602c
johnasharifi <johnasharifi@gmail.com>
2020-05-31 18:13:13 -0700

    Add world-scale filtered Heightmap demo scene
    Introduce a basic camera controller for facility.
    
    Modified the parameters of textures slightly, in order to better create maps.
    
    Re-introduced WorldScaleHeightmap.unity, which generates a large textured quad in the xy plane.

--- Scenes/Scripts/HeightmapToTexture.cs
+++ Scenes/Scripts/HeightmapToTexture.cs
@@ -16,22 +16,22 @@ public class HeightmapToTexture : MonoBehaviour
 
         var filter_water = MapFilterFactory.GetPerlinBand(dims, dims, 1.0f, 0.4f, 0.43f);
         var filter_exterior = MapFilterFactory.GetBlendedExteriorWeight(dims, dims, 0.5f);
-        var filter_lattice = MapFilterFactory.GetPerlinBand(dims, dims, 5.0f, 0.4f, 0.5f);
-        var filter_plains = MapFilterFactory.GetPerlinBand(dims, dims, 5.0f, 0.4f, 0.5f);
-        var filter_forests = MapFilterFactory.GetPerlinBand(dims, dims, 5.0f, 0.4f, 0.5f);
+        var filter_lattice = MapFilterFactory.GetPerlinBand(dims, dims, 10.0f, 0.3f, 0.4f);
         var filter_mountains = MapFilterFactory.GetPerlinBand(dims, dims, 5.0f, 0.4f, 0.5f);
+        var filter_forests = MapFilterFactory.GetPerlinBand(dims, dims, 10.0f, 0.4f, 0.5f);
+        var filter_plains = MapFilterFactory.GetPerlinBand(dims, dims, 5.0f, 0.4f, 0.5f);
 
         map.MapFromTo(-1, 5, -1, filter_water);
         map.MapFromTo(-1, 10, 1, filter_lattice);
         map.MapFromTo(1, 0, 1, filter_exterior);
-        
-        map.MapFromTo(1, 7, filter_plains);
-        map.MapFromTo(1, 8, filter_forests);
+
         map.MapFromTo(1, 9, filter_mountains);
+        map.MapFromTo(1, 8, filter_forests);
+        map.MapFromTo(1, 7, filter_plains);
         
         Dictionary<int, Color> mapping = new Dictionary<int, Color>()
         {
-            { 0, Color.black },
+            { 0, Color.red },
             { 5, Color.blue },
             { 7, Color.yellow },
             { 8, Color.green},
