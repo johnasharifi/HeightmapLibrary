cd3ee059cac8401b56f137a22639142aef6d6bbd
johnasharifi <johnasharifi@gmail.com>
2020-07-03 23:07:56 -0700

    Add stub scripts for pathfinding
    Currently MapPathfinder does not cache any info,
    
    does not perform any optimization (searches through cells in the sequence they are encountered in, ie bfs),
    
    and requires many assumptions about map and iteration limit. TODO fix

---
+++ Scenes/Scripts/PathfindingTotem.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[RequireComponent(typeof(LineRenderer))]
+public class PathfindingTotem : MonoBehaviour
+{
+    [SerializeField] private Renderer rend;
+    [SerializeField] private LineRenderer lrend;
+    [SerializeField] private Transform target;
+    [SerializeField] private Heightmap map;
+
+    [Range(64, 512)]
+    [SerializeField] private int dims;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        map = new Heightmap(dims, dims);
+        var filter_water = MapFilterFactory.GetPerlinBand(dims, dims, 1.0f, 0.4f, 0.43f);
+        var filter_exterior = MapFilterFactory.GetBlendedExteriorWeight(dims, dims, 0.5f);
+        var filter_lattice = MapFilterFactory.GetPerlinBand(dims, dims, 10.0f, 0.3f, 0.4f);
+        var filter_mountains = MapFilterFactory.GetPerlinBand(dims, dims, 5.0f, 0.4f, 0.5f);
+        var filter_forests = MapFilterFactory.GetPerlinBand(dims, dims, 10.0f, 0.4f, 0.5f);
+        var filter_plains = MapFilterFactory.GetPerlinBand(dims, dims, 5.0f, 0.4f, 0.5f);
+
+        map.MapFromTo(-1, 5, -1, filter_water);
+        map.MapFromTo(-1, 10, 1, filter_lattice);
+        map.MapFromTo(1, 0, 1, filter_exterior);
+
+        map.MapFromTo(1, 9, filter_mountains);
+        map.MapFromTo(1, 8, filter_forests);
+        map.MapFromTo(1, 7, filter_plains);
+
+
+        Dictionary<int, Color> mapping = new Dictionary<int, Color>()
+        {
+            { 0, Color.red },
+            { 5, Color.blue },
+            { 7, Color.yellow },
+            { 8, Color.green},
+            { 9, Color.red},
+            { 10, Color.white},
+            { -1, Color.white },
+            {1, Color.white }
+        };
+        HeightmapColorLookupTable lut = new HeightmapColorLookupTable(mapping);
+
+        Texture2D tex = map.AsTexture2D(lut);
+        tex.filterMode = FilterMode.Point;
+        tex.Apply();
+
+        rend.material.mainTexture = tex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            Tuple<int, int> origxz = new Tuple<int, int>(Mathf.FloorToInt(transform.position.x), Mathf.FloorToInt(transform.position.z));
+            Tuple<int, int> targetxz = new Tuple<int, int>(Mathf.FloorToInt(target.position.x), Mathf.FloorToInt(target.position.z));
+            System.Diagnostics.Stopwatch w = new System.Diagnostics.Stopwatch();
+            w.Start();
+            List<Tuple<int,int>> path = MapPathfinder.GetSlowFullPathFrom(null, origxz, targetxz);
+            w.Stop();
+            if (path == null || path.Count == 0)
+            {
+                Debug.LogFormat("no path found within {0} msec", w.ElapsedMilliseconds);
+                lrend.enabled = false;
+            }
+            else
+            {
+                lrend.enabled = true;
+                Vector3[] points = new Vector3[path.Count];
+                for (int i = 0; i < path.Count; i++)
+                {
+                    points[i] = new Vector3(path[i].Item1, 0f, path[i].Item2);
+                }
+                lrend.SetVertexCount(path.Count);
+                lrend.SetPositions(points);
+            }
+            
+        }
+    }
+}
---
+++ Scripts/MapPathfinder.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+public static class MapPathfinder
+{
+    // TODO stub; implement faster more general function later
+    public static List<Tuple<int,int>> GetSlowFullPathFrom(Heightmap map, Tuple<int,int> orig, Tuple<int, int> target, int cellSearchLimit = 128)
+    {
+        // inline function for getting cells adjacent to cell i,j in a grid
+        Func < Tuple<int, int>, IEnumerable <Tuple<int, int>>> GetAdjacent = (Tuple<int, int> p) =>
+        {
+            HashSet<Tuple<int, int>> items = new HashSet<Tuple<int, int>>();
+
+            // TODO convert to param later. need to retrieve from $map
+            const int maxDim = 128;
+
+            HashSet<Tuple<int, int>> collection = new HashSet<Tuple<int, int>>();
+            for (int i = p.Item1 - 1; i >= 0 && i < maxDim && i < p.Item1 + 2; i++)
+            {
+                for (int j = p.Item2 - 1; j >= 0 && j < maxDim && j < p.Item2 + 2; j++)
+                {
+                    // skip original point
+                    if (i == p.Item1 && j == p.Item2) { continue; };
+                    items.Add(new Tuple<int, int>(i, j));
+                }
+            }
+            return items;
+        };
+
+
+        // general objective:
+        // use cell grid search. BFS from $orig to $target.
+        // this pathfinding process will not work if we start from target and work backwards.
+
+        // a point can serve as both a lookup for whether a point has been visited, and a map to shortest known path
+        Dictionary<Tuple<int, int>, List<Tuple<int, int>>> shortestPath = new Dictionary<Tuple<int, int>, List<Tuple<int, int>>>();
+        // shortest path from $orig to $orig is empty list
+        shortestPath.Add(orig, new List<Tuple<int, int>>());
+        
+        Queue<Tuple<int, int>> pending = new Queue<Tuple<int, int>>();
+        pending.Enqueue(orig);
+        int iters = 0;
+        while (pending.Count > 0 && iters++ < cellSearchLimit)
+        {
+            Tuple<int,int> top = pending.Dequeue();
+            foreach (Tuple<int,int> adj in GetAdjacent(top))
+            {
+                if (shortestPath.ContainsKey(adj)) continue; // -> we have already encountered this path. assume we already have the shortest path
+                if (!shortestPath.ContainsKey(top)) continue; // -> we do not actually know a path to this cell
+
+                // the path to $adj is <path from orig to top> + <top>
+                // presumably we need to copy the list's elements using GetRange
+                shortestPath[adj] = shortestPath[top].GetRange(0, shortestPath[top].Count);
+                shortestPath[adj].Add(top);
+
+                if (shortestPath.ContainsKey(target))
+                {
+                    // if adj == target, then we already built the path
+                    return shortestPath[target];
+                }
+
+                // else add it to the stack of things to investigate
+                pending.Enqueue(adj);
+            }
+        }
+
+        // either no path, or no path found within time available
+        return null;
+    }
+}
