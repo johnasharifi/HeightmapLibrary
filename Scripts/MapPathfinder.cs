c419e4050bceaff02be55464b989e0dbe2f52346
johnasharifi <johnasharifi@gmail.com>
2020-07-04 23:46:10 -0700

    Add process for splitting map pathfinding and caching results
    Split i,j to n,m into (i,j to x,y) and (x,y to n,m)

--- Scripts/MapPathfinder.cs
+++ Scripts/MapPathfinder.cs
@@ -3,33 +3,81 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using System.Linq;
+// TODO alias tuple generics
+// using TupleInt2 = System.Tuple<int, int>;
+// using TupleInt4 = System.Tuple<int,int,int,int>;
 
 public static class MapPathfinder
 {
+    // approximately ideal path from cell i,j to cell n, m
+    // assumes only one heightmap, see param $map of Get*PathFrom
+    private static Dictionary<Tuple<int, int, int, int>, List<Tuple<int, int>>> recPathCache = new Dictionary<Tuple<int, int, int, int>, List<Tuple<int, int>>>();
+
+    public static List<Tuple<int,int>> GetFastApproximateFullPathFrom(Heightmap map, Tuple<int,int> orig, Tuple<int,int> target)
+    {
+        int maxDim = Mathf.Max(map.getDim(0), map.getDim(1));
+
+        // if already know path, return path
+        Tuple<int, int, int, int> p4 = new Tuple<int, int, int, int>(orig.Item1, orig.Item2, target.Item1, target.Item2);
+        if (recPathCache.ContainsKey(p4))
+        {
+            return recPathCache[p4];
+        }
+
+        int dist = Mathf.Abs(orig.Item1 - target.Item1) + Mathf.Abs(orig.Item2 - target.Item2);
+
+        // assume {fast && approximate} tolerates us returning literally any known path from orig -> intermediate, + intermediate -> target
+
+        // if distance longer than 2, split and recurse. 
+        // had to check distance > 2 instead of 1; with 1, encountered a stack overflow from cell i,j to cell (i+1),j
+        // cases: with dist > 2:
+        // i,j to (i+1),j is 1, which is not greater than 2
+        // i,j to (i+1),(j+1) is 1.41 which is not greater than 2
+        // i,j to (i+2),j is 2 which is not greater than 2
+        if (dist > 2)
+        {
+            Tuple<int, int> p_intermediate = new Tuple<int, int>((orig.Item1 + target.Item1) / 2, (orig.Item2 + target.Item2) / 2);
+            List<Tuple<int, int>> p1 = GetFastApproximateFullPathFrom(map, orig, p_intermediate);
+            List<Tuple<int, int>> p2 = GetFastApproximateFullPathFrom(map, p_intermediate, target);
+
+            // join two paths
+            List<Tuple<int, int>> combined = new List<Tuple<int, int>>(p1);
+            combined.AddRange(p2);
+            
+            // store in cache so we don't lose work. we only have to store path from ij to nm.
+            recPathCache[p4] = combined;
+
+            return combined;
+        }
+
+
+        // store in cache so we don't lose work
+        // implicit last case: adjacents, and no known path yet
+        return GetFastShortDistanceFrom(map, orig, target);
+    }
+
+    /// <summary>
+    /// Gets path from cell i,j to cell n,m where dist(i,j to n,m) < 1
+    /// </summary>
+    /// <param name="map">A heightmap with a max dimension</param>
+    /// <param name="orig">Origin cell i,j</param>
+    /// <param name="target">Target cell n,m</param>
+    /// <returns></returns>
+    private static List<Tuple<int,int>> GetFastShortDistanceFrom(Heightmap map, Tuple<int,int> orig, Tuple<int,int> target)
+    {
+        // path from i,j to adjacent n,m is simply i,j
+        return new List<Tuple<int, int>> { orig };
+
+        // this function will be extended later to support caching even when distances > 1, and better solutions even when i,j is
+        // within distance 1 but not adjacent (due to cells being unpathable)
+    }
+
+
     // TODO stub; implement faster more general function later
     public static List<Tuple<int,int>> GetSlowFullPathFrom(Heightmap map, Tuple<int,int> orig, Tuple<int, int> target, int cellSearchLimit = 128)
     {
         // param from $map
         int maxDim = Mathf.Max(map.getDim(0), map.getDim(1));
-
-        // inline function for getting cells adjacent to cell i,j in a grid
-        Func< Tuple<int, int>, IEnumerable <Tuple<int, int>>> GetAdjacent = (Tuple<int, int> p) =>
-        {
-            HashSet<Tuple<int, int>> items = new HashSet<Tuple<int, int>>();
-
-            HashSet<Tuple<int, int>> collection = new HashSet<Tuple<int, int>>();
-            for (int i = p.Item1 - 1; i >= 0 && i < maxDim && i < p.Item1 + 2; i++)
-            {
-                for (int j = p.Item2 - 1; j >= 0 && j < maxDim && j < p.Item2 + 2; j++)
-                {
-                    // skip original point
-                    if (i == p.Item1 && j == p.Item2) { continue; };
-                    items.Add(new Tuple<int, int>(i, j));
-                }
-            }
-            return items;
-        };
-
 
         // general objective:
         // use cell grid search. BFS from $orig to $target.
@@ -46,7 +94,7 @@ public static class MapPathfinder
         while (pending.Count > 0 && iters++ < cellSearchLimit)
         {
             Tuple<int,int> top = pending.Dequeue();
-            foreach (Tuple<int,int> adj in GetAdjacent(top))
+            foreach (Tuple<int,int> adj in GetAdjacent(maxDim, top))
             {
                 if (shortestPath.ContainsKey(adj)) continue; // -> we have already encountered this path. assume we already have the shortest path
                 if (!shortestPath.ContainsKey(top)) continue; // -> we do not actually know a path to this cell
@@ -69,5 +117,29 @@ public static class MapPathfinder
 
         // either no path, or no path found within time available
         return null;
+    }
+    
+    // TODO cache adjacency data; currently we do n^2 iteration and new() each time, but could cache to avoid impact
+    /// <summary>
+    /// Defines adjacency for a cell i,j in a grid of integer-discretized cells
+    /// </summary>
+    /// <param name="maxDim">Cell grid size</param>
+    /// <param name="p">A point i,j in grid</param>
+    /// <returns>A collection of cells adjacent to cell i,j</returns>
+    static IEnumerable<Tuple<int, int>> GetAdjacent (int maxDim, Tuple<int, int> p)
+    {
+        HashSet<Tuple<int, int>> items = new HashSet<Tuple<int, int>>();
+
+        HashSet<Tuple<int, int>> collection = new HashSet<Tuple<int, int>>();
+        for (int i = p.Item1 - 1; i >= 0 && i < maxDim && i < p.Item1 + 2; i++)
+        {
+            for (int j = p.Item2 - 1; j >= 0 && j < maxDim && j < p.Item2 + 2; j++)
+            {
+                // skip original point
+                if (i == p.Item1 && j == p.Item2) { continue; };
+                items.Add(new Tuple<int, int>(i, j));
+            }
+        }
+        return items;
     }
 }
