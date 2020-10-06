7be30984fa02a64f77067d50c64fee903221f7fe
johnasharifi <johnasharifi@gmail.com>
2020-10-05 22:01:42 -0700

    Introduce a serializable HeightmapBiomeFilterLookupTable
    HeightmapBiomeFilterLookupTable will wrap around List<HeightmapBiomeFilter> to circumvent Unity's JsonUtility issue with serializing Lists and Arrays.
    
    This object will have parity with SpeedLookupTable and ColorLookupTable.

---
+++ Scripts/HeightmapBiomeFilterLookupTable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class HeightmapBiomeFilterLookupTable
+{
+    [SerializeField] private List<HeightmapBiomeFilter> filters = new List<HeightmapBiomeFilter>();
+
+    public void Add(HeightmapBiomeFilter filter)
+    {
+        filters.Add(filter);
+    }
+
+    public HeightmapBiomeFilter this[int i]
+    {
+        get
+        {
+            return filters[i];
+        }
+    }
+}
