f981f1da71299d25638586516ea75f009a42405e
johnasharifi <johnasharifi@gmail.com>
2021-02-01 23:53:02 -0800

    Revert "Implement GetBiomeAdjacentMismatches"
    This reverts commit 4de0a03ed11cc9c6d88793fe4acaffb4ff3ffe26.

--- Scenes/Scripts/BiomeToFloodfilledMesh.cs
+++ Scenes/Scripts/BiomeToFloodfilledMesh.cs
@@ -58,30 +58,6 @@ public class BiomeToFloodfilledMesh : MonoBehaviour
         // return the terminal Tuple that pairs with the initial Tuple to form a rect
         return new Tuple<int, int>(initPoint.Item1 + Mathf.Max(0, colSpan - 1), initPoint.Item2 + Mathf.Max(0, rowSpan - 1));
     }
-
-    static List<Tuple<int, int>> GetBiomeAdjacentMismatches(Tuple<int, int> origin, Heightmap originalMap)
-    {
-        // given a point $origin, test origin -> up, origin -> right, origin -> down, origin -> left
-        // record biome mismatches so we can draw height transitions
-
-        // inline test for point containment. Might be good to move later
-        Func<Tuple<int, int>, bool> isPointInGrid = (Tuple<int, int> ij) =>
-        {
-            return -1 < ij.Item1 && ij.Item1 < originalMap.getDim(0) && -1 < ij.Item2 && ij.Item2 < originalMap.getDim(1);
-        };
-
-        List<Tuple<int,int>> mismatches = new List<Tuple<int, int>>();
-
-        // might only have to run this process for 50% of our points since each point tests all adjacent points
-        foreach (Tuple<int,int> offsetPoint in new[] {new Tuple<int,int>(origin.Item1 + 1, origin.Item2), new Tuple<int,int>(origin.Item1, origin.Item2), new Tuple<int,int>(origin.Item1 - 1, origin.Item2), new Tuple<int, int>(origin.Item1, origin.Item2 - 1) })
-        {
-            if (isPointInGrid(offsetPoint) && originalMap[offsetPoint.Item1, offsetPoint.Item2] != originalMap[origin.Item1, origin.Item2])
-            {
-                mismatches.Add(offsetPoint);
-            }
-        }
-        return mismatches;
-    }
     
     public static Dictionary<int, Mesh> GetMeshesFromHeightmap(Heightmap heightmapWithBiomes)
     {
@@ -100,16 +76,6 @@ public class BiomeToFloodfilledMesh : MonoBehaviour
             
             foreach (Tuple<int,int> point in heightmapWithBiomes[biome])
             {
-                if (point.Item1 % 2 == 0 && point.Item2 % 2 == 0)
-                {
-                    // then it's an "even" cell. run adjacency queries on these cells
-                    List<Tuple<int,int>> mismatches = GetBiomeAdjacentMismatches(point, heightmapWithBiomes);
-                    if (mismatches.Count > 0)
-                    {
-                        // TODO
-                    }
-                }
-
                 if (!meshes.ContainsKey(biome) || meshes[biome] == null)
                 {
                     meshes[biome] = new Mesh();
