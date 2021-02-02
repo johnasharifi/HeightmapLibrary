f50606cd7a9b6501803ff7399270b7641f2598d5
johnasharifi <johnasharifi@gmail.com>
2021-02-02 00:41:50 -0800

    Tune heights
    Squash to -1 to +1 range

--- Scripts/Heightmap.cs
+++ Scripts/Heightmap.cs
@@ -82,11 +82,11 @@ public class Heightmap : MonoBehaviour
             // special cases: 
             // forests are +3, mountains are +6
             // deserts are -1, rivers are -3
-            table[0] = 6.0f;
-            table[9] = 6.0f;
-            table[8] = 3.0f;
-            table[7] = -1.0f;
-            table[5] = -3.0f;
+            table[0] = 2.0f;
+            table[9] = 2.0f;
+            table[8] = 1.0f;
+            table[7] = -0.5f;
+            table[5] = -1.0f;
             return table;
         }
     }
